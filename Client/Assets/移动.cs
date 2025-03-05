370d55972ffa1e22b48108ae965bf4a37e451442
LEE24604 <148571859+LEE24604@users.noreply.github.com>
2025-03-05 17:18:26 +0800

    Add files via upload
    游戏基本文件

---
+++ Client/Assets/移动.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Move
+{
+    public Move(float xx,float yy)
+    { x = xx;y = yy; }
+
+    public float x;
+    public float y;
+}
+
+public class 移动 : MonoBehaviour
+{
+    NetWork network;
+    public float speed;
+    public KeyCode LeftKey, RightKey; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        network = GameObject.Find("NetWork").GetComponent<NetWork>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(LeftKey))
+        {
+            transform.position += new Vector3(speed * -1,0,0);
+        }
+        if (Input.GetKey(RightKey))
+        {
+            transform.position += new Vector3(speed,0,0);
+        }
+        network.Send<Move>(new Move(transform.position.x,transform.position.y));
+    }
+}
